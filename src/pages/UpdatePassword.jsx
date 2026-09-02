a433a7f6b6090bc544f139d876d257b14de1f97b
Kartik sahu <kartik.sahu3311@gmail.com>
2026-09-02 11:52:21 +0530

    Normalize password reset token from URL

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -30,7 +30,7 @@ function UpdatePassword() {
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
-        const token = location.pathname.split("/").at(-1)
+        const token = decodeURIComponent(location.pathname.split("/").filter(Boolean).pop() || "").trim()
         dispatch(resetPassword(password, confirmPassword, token, navigate))
     }
 
